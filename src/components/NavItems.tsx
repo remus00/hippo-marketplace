c96f9b3f8667932078358b36914f96c4b4d3ff8e
remus00 <135813551+remus00@users.noreply.github.com>
2023-11-27 21:55:29 +0100

    Navbar closed on outside click or escape key click

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -2,15 +2,32 @@
 import { PRODUCT_CATEGORIES } from '@/config';
 import { useState, useRef, useEffect } from 'react';
 import NavItem from './NavItem';
-// import { useOnClickOutside } from '@/hooks/use-on-click-outside';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
 
 const NavItems = () => {
     const [activeIdex, setActiveIndex] = useState<null | number>(null);
 
     const isAnyOpen = activeIdex !== null;
 
+    const navRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const handler = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                setActiveIndex(null);
+            }
+        };
+        document.addEventListener('keydown', handler);
+
+        return () => {
+            document.removeEventListener('keydown', handler);
+        };
+    }, []);
+
+    useOnClickOutside(navRef, () => setActiveIndex(null));
+
     return (
-        <div className="flex gap-4 h-full">
+        <div className="flex gap-4 h-full" ref={navRef}>
             {PRODUCT_CATEGORIES.map((category, index) => {
                 const handleOpen = () => {
                     if (activeIdex === index) {
